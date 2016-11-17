b0b8d9bc17a465e852b13f8d73a6f3d10c10e141
canyoucsharp <mkholodovsky@gmail.com>
2016-11-17 12:57:10 -0500

    insertion fix

--- insertion.cpp
+++ insertion.cpp
@@ -2,20 +2,26 @@
 
 void Sorting:: insertion_sort(int arr[], int length)//mit insertion sort implementation
 {
-	
+	int numofsteps=0;
 	
     int j, k;
 	int temp;
 
+	numofsteps++;  //j=1
 	for (j = 1; j < length; j++)
 	{
+		numofsteps = numofsteps + 4;//j < length; j++; temp = arr[j]; k = j - 1;
 		temp = arr[j];
 		k = j - 1;
 		while (k >= 0 && arr[k] > temp)
 		{
 			arr[k + 1] = arr[k];
 			k--;
+			numofsteps = numofsteps + 4;//
 		}
+		numofsteps = numofsteps + 3;//
 		arr[k + 1] = temp;
 	}
+	numofsteps++;//j < length;
 }
+
