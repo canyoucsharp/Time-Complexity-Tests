907f645ea9bdb6fd9189dae6cf22afe5c2d2c824
dennikra <denniskravtsov95@gmail.com>
2016-12-01 16:05:52 -0500

    Merge sort fixed + counting

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -74,3 +74,87 @@ void Sorting::mergeSort(int arr[], int l, int r)
 		merge(arr, l, m, r);
 	}
 }
+
+void Sorting::merge1(int arr[], int l, int m, int r)
+{
+	int i, j, k, n1 = m - l + 1, n2 = r - m;
+
+	/* create temp arrays */
+	int *L = new int[n1], *R = new int[n2];
+	FinCount++;
+
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+	{
+		L[i] = arr[l + i];
+		FinCount += 2;
+	}
+	for (j = 0; j < n2; j++)
+	{
+		R[j] = arr[m + 1 + j];
+		FinCount += 2;
+	}
+
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray'
+	FinCount += 3;
+	while (i < n1 && j < n2)
+	{
+		FinCount += 2;
+		if (L[i] <= R[j])
+		{
+			arr[k] = L[i];
+			i++;
+			FinCount += 2;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+			FinCount += 2;
+		}
+		k++;
+		FinCount++;
+	}
+
+	/* Copy the remaining elements of L[], if there
+	       are any */
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+		FinCount += 4;
+	}
+
+	/* Copy the remaining elements of R[], if there
+	       are any */
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+		FinCount += 4;
+	}
+}
+
+void Sorting::mergesort2(int arr[], int l, int r)
+{
+	if (l < r)
+	{
+		FinCount++;
+		// Same as (l+r)/2, but avoids overflow for
+		// large l and h
+		int m = l + (r - l) / 2;
+		FinCount++;
+		// Sort first and second halves
+		mergesort2(arr, l, m);
+		FinCount++;
+		mergesort2(arr, m + 1, r);
+		FinCount++;
+		merge1(arr, l, m, r);
+		FinCount++;
+	}
+}
