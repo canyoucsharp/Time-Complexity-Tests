e611804b35c401fd2a2d63adaa80f23b1e1420a7
BrianoBruno <briano.bruno@cix.csi.cuny.edu>
2016-12-01 23:31:29 -0500

    Case 1 Insertion Sort Finished

--- Print.cpp
+++ Print.cpp
@@ -61,18 +61,27 @@ void Sorting::PHead(int SortIdentifier)
 {
 
 	if (SortIdentifier == 1 && Ins == false)
+	{
+		Ins = true;
 		InsertionFile << "Case,Size,Steps,Time,\n";
-
+	}
 	else if (SortIdentifier == 2 && Sel == false)
+	{
+		Sel = true;
 		SelectionFile << "Case,Size,Steps,Time,\n";
-
-	else if (SortIdentifier == 3 && Bub == false)
+	}
+	else if (SortIdentifier == 3 && Bub == false) {
+		Bub = true;
 		BubbleFile << "Case,Size,Steps,Time,\n";
+	}
 
-	else if (SortIdentifier == 4 && Mer == false)
+	else if (SortIdentifier == 4 && Mer == false) {
+		Mer = true;
 		MergeFile << "Case,Size,Steps,Time,\n";
-
-	else if (SortIdentifier == 5 && Quic == false)
+	}
+	else if (SortIdentifier == 5 && Quic == false) {
+		Quic = true;
 		QuickFile << "Case,Size,Steps,Time,\n";
+	}
 }
 
--- test.cpp
+++ test.cpp
@@ -39,10 +39,35 @@ void Sorting::test()
 
 	int cs = 1;
 	case1();
+
+	//Insertion Sort Case 1
 	FinCount=insertion_sortSTEPS(a100,100);
 	FinTime=insertion_sortTime(a100, 100);
 	Print(1, 100, cs);
+	FinCount = insertion_sortSTEPS(a200, 200);
+	FinTime = insertion_sortTime(a200, 200);
+	Print(1, 200, cs);
+	FinCount = insertion_sortSTEPS(a300,300);
+	FinTime = insertion_sortTime(a300, 300);
+	Print(1, 300, cs);
+	FinCount = insertion_sortSTEPS(a400,400);
+	FinTime = insertion_sortTime(a400, 400);
+	Print(1, 400, cs);
+	FinCount = insertion_sortSTEPS(a500, 500);
+	FinTime = insertion_sortTime(a500, 500);
+	Print(1, 500, cs);
+	FinCount = insertion_sortSTEPS(a1000, 1000);
+	FinTime = insertion_sortTime(a1000, 1000);
+	Print(1, 1000, cs);
+	FinCount = insertion_sortSTEPS(a2000, 2000);
+	FinTime = insertion_sortTime(a2000, 2000);
+	Print(1, 2000, cs);
+	FinCount = insertion_sortSTEPS(a4000, 4000);
+	FinTime = insertion_sortTime(a4000, 4000);
+	Print(1, 4000, cs);
+	FinCount = insertion_sortSTEPS(a10000, 10000);
+	FinTime = insertion_sortTime(a10000, 10000);
+	Print(1, 10000, cs);
 
-	cout << "Time = " << FinTime << endl;
-	cout << "Steps = " << FinCount << endl;
+
 }
