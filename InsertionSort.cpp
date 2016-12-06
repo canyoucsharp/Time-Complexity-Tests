bec41d257299f69195dd4cc731fc4993eb5a1248
BrianoBruno <briano.bruno@cix.csi.cuny.edu>
2016-12-06 11:21:05 -0500

    Removed hold

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -2,23 +2,22 @@
 
 int Sorting:: insertion_sortSTEPS(int arr[], int length)//mit insertion sort implementation
 {
-	int * hold = arr;
 	int numofsteps=0, j, k, temp;
 
 	numofsteps++;  //j=1
 	for (j = 1; j < length; j++)
 	{
 		numofsteps = numofsteps + 4;//j < length; j++; temp = hold[j]; k = j - 1;
-		temp = hold[j];
+		temp = arr[j];
 		k = j - 1;
-		while (k >= 0 && hold[k] > temp)
+		while (k >= 0 && arr[k] > temp)
 		{
-			hold[k + 1] = hold[k];
+			arr[k + 1] = arr[k];
 			k--;
 			numofsteps = numofsteps + 4;//
 		}
 		numofsteps = numofsteps + 3;//
-		hold[k + 1] = temp;
+		arr[k + 1] = temp;
 	}
 	numofsteps++;//j < length;
 
@@ -27,8 +26,6 @@ int Sorting:: insertion_sortSTEPS(int arr[], int length)//mit insertion sort imp
 
 double Sorting::insertion_sortTime(int arr[], int length)//returns the amount of time it takes too run the program
 {
-	
-	int * hold = arr;
 	
 
 	clock_t start, end;
@@ -42,16 +39,16 @@ double Sorting::insertion_sortTime(int arr[], int length)//returns the amount of
 	for (j = 1; j < length; j++)
 	{
 	
-		temp = hold[j];
+		temp = arr[j];
 		k = j - 1;
-		while (k >= 0 && hold[k] > temp)
+		while (k >= 0 && arr[k] > temp)
 		{
-			hold[k + 1] = hold[k];
+			arr[k + 1] = arr[k];
 			k--;
 			
 		}
 		
-		hold[k + 1] = temp;
+		arr[k + 1] = temp;
 	}
 	end= clock();
 	
